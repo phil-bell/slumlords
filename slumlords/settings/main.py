57a01c600326649e5366c2dc3f4cb5c2c9d64a50
phil-bell <philhabell@gmail.com>
2021-01-24 13:44:58 +0000

    adding env for secret

--- slumlords/settings/main.py
+++ slumlords/settings/main.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,6 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 ALLOWED_HOSTS = []
+
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "")
 
 
 # Application definition
