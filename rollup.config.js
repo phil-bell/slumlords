2160d7958054e342e878ab67aa39dac78314eb72
phil-bell <philhabell@gmail.com>
2021-02-14 21:37:39 +0000

    getting map working

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,25 @@
+// export default {
+//     input: 'slumlords/static-src/js/main.js',
+//     output: {
+//       file: 'slumlords/static/js/bundle.js',
+//       format: 'umd',
+//     },
+//     inlineDynamicImports: true,
+//   };
+
+import resolve from 'rollup-plugin-node-resolve';
+
 export default {
-    input: 'slumlords/static-src/js/main.js',
-    output: {
-      file: 'slumlords/static/js/bundle.js',
-      format: 'iife'
-    }
-  };
+  // If using any exports from a symlinked project, uncomment the following:
+	input: 'slumlords/static-src/js/main.js',
+	output: {
+    file: 'slumlords/static/js/bundle.js',
+		format: 'es',
+		sourcemap: true
+	},
+  preserveSymlinks: true,
+  inlineDynamicImports: true,
+	plugins: [
+    resolve()
+  ]
+};
