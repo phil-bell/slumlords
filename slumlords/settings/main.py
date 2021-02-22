ebcb41e019de22c384a59092aca12ee7b908b674
phil-bell <philhabell@gmail.com>
2021-02-22 23:53:46 +0000

    Creating models

--- slumlords/settings/main.py
+++ slumlords/settings/main.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "corsheaders",
+    "address",
     "slumlords.api",
     "slumlords.apps.home",
     "slumlords.apps.map",
