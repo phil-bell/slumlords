db2ccd189b36a7ca12cf60e6c33dad588a1eae86
phil-bell <philhabell@gmail.com>
2021-03-28 19:39:54 +0100

    fixing backend form submission

--- slumlords/apps/reviews/views.py
+++ slumlords/apps/reviews/views.py
@@ -7,9 +7,6 @@ from geopy.geocoders import Nominatim
 from .forms import ReviewForm
 from .models import Landlord, Property
 
-geolocator = Nominatim(user_agent="slumlords")
-
-
 class ReviewCreateView(TemplateView):
     template_name = "reviews/review_form.html"
 
@@ -25,11 +22,12 @@ class ReviewCreateView(TemplateView):
 
     @landlord.setter
     def landlord(self, review):
-        self._landlord = Landlord.objects.get_or_create(
+        obj, created = Landlord.objects.get_or_create(
             first_name=review.cleaned_data.get("landlord_first_name"),
             last_name=review.cleaned_data.get("landlord_last_name"),
             postcode=review.cleaned_data.get("landlord_postcode"),
         )
+        self._landlord = obj
 
     @property
     def rental(self):
@@ -37,12 +35,10 @@ class ReviewCreateView(TemplateView):
 
     @rental.setter
     def rental(self, review):
-        self._rental = Property.objects.get_or_create(
+        obj, created = Property.objects.get_or_create(
             address=review.cleaned_data.get("property_address"), landlord=self.landlord
         )
-        location = geolocator.geocode(self._rental.address.raw)
-        self._rental.latitude = location.latitude
-        self._rental.longatude = location.longitude
+        self._rental = obj
         self._rental.save()
 
     def get_context_data(self, **kwargs):
@@ -51,13 +47,12 @@ class ReviewCreateView(TemplateView):
         return context
 
     def post(self, request):
-        self.review = ReviewForm(request.POST)
+        self.review = ReviewForm(json.loads(request.body.decode("utf-8")))
         if self.review.is_valid():
             self.landlord = self.review
             self.rental = self.review
             self.review.rental = self.rental
-            self.review.tenent = self.request.user.tenent
+            # self.review.tenent = self.request.user.tenent
             self.review.save()
             return HttpResponse()
-        print(self.review.errors.as_json())
         return JsonResponse(data=self.review.errors, safe=False, status=400)
