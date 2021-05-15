1da88f7752e6f7ccd4c1287615042f1e0222180f
phil-bell <philhabell@gmail.com>
2021-05-15 20:01:06 +0100

    creating landlord endpoint

--- slumlords/apps/review/serializers.py
+++ slumlords/apps/review/serializers.py
@@ -1,9 +1,21 @@
 from rest_framework import serializers
 
-from .models import Review
+from .models import Landlord, Review
 
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = ["rating", "submitted_at", "description", "rent"]
+
+
+class LandlordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Landlord
+        fields = [
+            "first_name",
+            "last_name",
+            "postcode",
+            "rating",
+        ]
+
--- slumlords/apps/review/views/api.py
+++ slumlords/apps/review/views/api.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets, mixins
 
-from ..serializers import ReviewSerializer
-from ..models import Review
+from ..serializers import LandlordSerializer, ReviewSerializer
+from ..models import Landlord, Review
 
 
 class ReviewViewSet(
@@ -12,3 +12,12 @@ class ReviewViewSet(
 ):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+
+class LandlordViewset(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Landlord.objects.all()
+    serializer_class = LandlordSerializer
